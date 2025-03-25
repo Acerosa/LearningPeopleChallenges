c8777bf2ec468dc6f89ee0551089962adb470816
ricardorosa <rjrosa@outlook.com>
2025-03-25 13:48:45 +0000

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { useState } from 'react'
-import ProductCard from './ProductCard'
+import Productcard from './ProductCard'
 import './App.css'
 
 
 function App() {
-  
   return (
     <>
- 
+      <Productcard
+        title="Cool Shoes"
+        price="$49.99"
+        desc="Comfortable and stylish shoes for everyday wear."
+        image="broken-url.jpg"
+      />
     </>
   )
-
 }
 
-export default App
+export defautl App
--- src/ProductCard.jsx
+++ src/ProductCard.jsx
@@ -1,15 +1,14 @@
-const PoductCard = () => {
+const PrductCard = ({ title, price, description, image }) => {
   return (
     <div clasName="product-card">
       <img
-        src="https://jcpenney.scene7.com/is/image/JCPenney/DP0511202307052984M?resmode=sharp2&op_sharpen=1&wid=150&hei=150"
-        atl="Cool Shoes"
-        clasName="product-image"
+        src={image}
+        alt={title}
+        className="product-image"
       />
-      <h2>Cool Shoes</h2>
-      <p clasName="price">$49.99</p>
-      <p>Comfortable and stylish shoes for everyday wear. Perfect for walking, running, or chilling.</p>
+      <h2>{title}</h2>
+      <p className="price">{price}</p>
+      <p>{descriptin}</p>
     </div>
   );
-};
-
+}
